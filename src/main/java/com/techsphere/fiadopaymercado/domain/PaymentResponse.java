d80612268e9175bd43cbd62392a8cdbe9d2d281b
leticia-farias <leticiafarias.engsoftware@gmail.com>
2025-11-21 19:53:30 -0300

    refactor: volta PaymentResponse para record

--- src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
+++ src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
@@ -1,73 +1,15 @@
 package com.techsphere.fiadopaymercado.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 
-public class PaymentResponse {
-
-    private String id;
-    private String status;
-    private String method;
-    private BigDecimal amount;
-    private Integer installments;
-    private BigDecimal total;
-
-    public PaymentResponse(String id, String status, String method, BigDecimal amount, Integer installments, BigDecimal total) {
-        this.id = id;
-        this.status = status;
-        this.method = method;
-        this.amount = amount;
-        this.installments = installments;
-        this.total = total;
-    }
-
-    public PaymentResponse() {
-	}
-
-	public String getId() {
-        return id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public Integer getInstallments() {
-        return installments;
-    }
-
-    public BigDecimal getTotal() {
-        return total;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public void setInstallments(Integer installments) {
-        this.installments = installments;
-    }
-
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-}
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record PaymentResponse(
+    String id,
+    String status,
+    String method,
+    BigDecimal amount,
+    Integer installments,
+    BigDecimal total,
+    BigDecimal interestRate
+) {}
