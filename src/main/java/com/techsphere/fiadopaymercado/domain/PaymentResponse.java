b75da3473acd7e777daf8b8ee0a02257531684a6
RizzoKa <kauarizzo06@gmail.com>
2025-11-21 17:02:09 -0300

    refactor: mudando para classe paymentresponse

--- src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
+++ src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
@@ -2,13 +2,72 @@ package com.techsphere.fiadopaymercado.domain;
 
 import java.math.BigDecimal;
 
-public record PaymentResponse(String id, String status, String method, BigDecimal amount, Integer installments, BigDecimal total) {
+public class PaymentResponse {
 
-	public String getStatus() {
-		return status;
+    private String id;
+    private String status;
+    private String method;
+    private BigDecimal amount;
+    private Integer installments;
+    private BigDecimal total;
+
+    public PaymentResponse(String id, String status, String method, BigDecimal amount, Integer installments, BigDecimal total) {
+        this.id = id;
+        this.status = status;
+        this.method = method;
+        this.amount = amount;
+        this.installments = installments;
+        this.total = total;
+    }
+
+    public PaymentResponse() {
 	}
 
 	public String getId() {
-		return id;
-	}
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Integer getInstallments() {
+        return installments;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public void setInstallments(Integer installments) {
+        this.installments = installments;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
 }
