211befc6d2adeebd8985cf17491fdf6447cd9dbe
RizzoKa <kauarizzo06@gmail.com>
2025-11-21 15:43:50 -0300

    feat: metodos get em paymentresponse

--- src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
+++ src/main/java/com/techsphere/fiadopaymercado/domain/PaymentResponse.java
@@ -2,4 +2,13 @@ package com.techsphere.fiadopaymercado.domain;
 
 import java.math.BigDecimal;
 
-public record PaymentResponse(String id, String status, String method, BigDecimal amount, Integer installments, BigDecimal total) {}
+public record PaymentResponse(String id, String status, String method, BigDecimal amount, Integer installments, BigDecimal total) {
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getId() {
+		return id;
+	}
+}
