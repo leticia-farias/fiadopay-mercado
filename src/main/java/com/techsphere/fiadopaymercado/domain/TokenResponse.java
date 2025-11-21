509b912936c9b5e192a31eef0e8a42e1dcf06e49
AlannaFM <alannamachado84@gmail.com>
2025-11-21 09:58:29 -0300

    feat: anotação em TokenResponse

--- src/main/java/com/techsphere/fiadopaymercado/domain/TokenResponse.java
+++ src/main/java/com/techsphere/fiadopaymercado/domain/TokenResponse.java
@@ -1,3 +1,6 @@
 package com.techsphere.fiadopaymercado.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record TokenResponse(String access_token, String token_type, long expires_in) {}
